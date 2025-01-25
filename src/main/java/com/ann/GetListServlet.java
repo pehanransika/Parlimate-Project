5d091a863b85deaf00beaae94f6e79463d503122
username <himashachinthani@gmail.com>
2025-01-25 11:27:06 +0530

    assd

--- src/main/java/com/ann/GetListServlet.java
+++ src/main/java/com/ann/GetListServlet.java
@@ -1,20 +1,16 @@
 package com.ann;
 
-
 import UserPackage.PoliticianModel;
 import UserPackage.UserModel;
 
 import javax.net.ssl.HandshakeCompletedEvent;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import javax.servlet.http.HttpSession;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -31,13 +27,6 @@ public class GetListServlet extends HttpServlet {
 // Then forward to announcementsDetail.jsp
         RequestDispatcher dispatcher2 = request.getRequestDispatcher("announcementlist.jsp");
         dispatcher2.include(request, response);
-
-// Forward to announcements.jsp
-        RequestDispatcher dispatcher1 = request.getRequestDispatcher("announcementlist.jsp");
-        dispatcher1.include(request, response);
-
-// Then forward to announcementsDetail.jsp
-
 
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
